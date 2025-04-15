52dae6139273b4d01254fe33d8ed65ab7f558853
Mohaymen Omar <mohaymenomar13@gmail.com>
2025-04-15 11:04:02 +0800

    Adding Admin Dashboard

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -21,6 +21,16 @@ const routes: RouteRecordRaw[] = [
       }
     ]
   },
+  {
+    path: '/admin',
+    component: () => import('layouts/AdminLayout.vue'),
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('pages/admin/DashboardPage.vue')
+      }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
