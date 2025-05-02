3203074578972a3f4ce1004789c828752f3926bf
fencim <fencim@gmail.com>
2025-05-02 17:01:10 +0800

    confirm ples

--- quasar.config.ts
+++ quasar.config.ts
@@ -116,7 +116,8 @@ export default defineConfig((ctx) => {
       // Quasar plugins
       plugins: [
         'LocalStorage',
-        'SessionStorage'
+        'SessionStorage',
+        'Notify'
       ]
     },
 
