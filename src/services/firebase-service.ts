810f160fd5f6f30c3e0cd296d3ab23fd87c26356
fencim <fencim@gmail.com>
2025-05-03 23:06:36 +0800

    setup store + service for auth with firebase

--- src/services/firebase-service.ts
+++ src/services/firebase-service.ts
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, User } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,6 +18,64 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-console.log(analytics);
+class FirebaseService {
+  /**
+   * https://firebase.google.com/docs/auth/web/google-signin
+   */
+  async signInWithGoogle() {
+    const authentication = await signInWithPopup(auth, googleProvider);
+    return authentication.user;
+  }
+  /**
+   * https://firebase.google.com/docs/auth/web/password-auth
+   * @param email
+   * @param password
+   */
+  async registerWithEmailPassword(email: string, password: string) {
+    const credential = await createUserWithEmailAndPassword(auth, email, password);
+    return credential.user;
+  }
+  /**
+   * https://firebase.google.com/docs/auth/web/password-auth
+   * @param email
+   * @param password
+   */
+  async signWithEmailPassword(email: string, password: string) {
+    const credential = await signInWithEmailAndPassword(auth, email, password);
+    return credential.user;
+  }
+  signOut() {
+    return signOut(auth)
+  }
+  /**
+   * https://firebase.google.com/docs/auth/web/start
+   * @returns User
+   */
+  async authorizeUser() {
+    return new Promise<User>((resolve) => {
+      if (auth.currentUser) {
+        resolve(auth.currentUser);
+      } else {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            resolve(user);
+          } else {
+            // User is signed out
+            // ...
+          }
+        });
+      }
+    })
+  }
+  resetPassword(email: string) {
+    return sendPasswordResetEmail(auth, email);
+  }
+
+}
+
+export const firebaseService = new FirebaseService();
